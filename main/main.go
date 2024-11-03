65a4a7875133b788074d1418a58e4600a9c20070
lsilvpin <lsilvpin@gmail.com>
2024-11-03 02:31:29 -0300

    Ajuste para carregar arquivo .env

--- main/main.go
+++ main/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/lsilvpin/goecommerce-model-api/main/entrypoint"
+	"github.com/lsilvpin/goecommerce-model-api/main/library/tools"
 	"github.com/lsilvpin/goecommerce-model-api/main/library/utils"
 )
 
 func main() {
+	tools.LoadDotEnv()
 	utils.ConnectToDatabaseAndMigrate()
 	entrypoint.SetupApi()
 }
