22c821322ae2c7e0c47f7cf63650014ee78b2272
lsilvpin <lsilvpin@gmail.com>
2024-11-03 02:43:14 -0300

    Conexão com sqlite implementada para ambiente de dev

--- go.mod
+++ go.mod
@@ -5,12 +5,21 @@ go 1.22.7
 require github.com/gin-gonic/gin v1.10.0
 
 require (
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/glebarez/go-sqlite v1.21.2 // indirect
+	github.com/glebarez/sqlite v1.11.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.5.5 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	golang.org/x/sync v0.8.0 // indirect
+	modernc.org/libc v1.22.5 // indirect
+	modernc.org/mathutil v1.5.0 // indirect
+	modernc.org/memory v1.5.0 // indirect
+	modernc.org/sqlite v1.23.1 // indirect
 )
 
 require (
--- main/library/utils/database_connector.go
+++ main/library/utils/database_connector.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"log"
 
+	"github.com/glebarez/sqlite"
 	entities "github.com/lsilvpin/goecommerce-model-api/main/library/domain/entities"
 	"github.com/lsilvpin/goecommerce-model-api/main/library/tools"
 	"gorm.io/driver/postgres"
@@ -14,10 +15,17 @@ var (
 )
 
 func ConnectToDatabaseAndMigrate() {
+	environment := tools.GetEnv("ENVIRONMENT")
 	connectionString := tools.GetEnv("DATABASE_CONNECTION_STRING")
 	log.Println("Connection string: ", connectionString)
 	err := error(nil)
-	DB, err = gorm.Open(postgres.Open(connectionString))
+	if environment == "dev" {
+		DB, err = gorm.Open(sqlite.Open(".tmp/db.sqlite"), &gorm.Config{})
+		log.Println("Connected to sqlite")
+	} else {
+		DB, err = gorm.Open(postgres.Open(connectionString))
+		log.Println("Connected to postgres")
+	}
 	if err != nil {
 		log.Panic("Failed to connect to database")
 	}
